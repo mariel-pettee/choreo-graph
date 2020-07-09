192f5b4502507db38691bc4555a611844eac4ee3
Mariel Pettee <mariel.pettee@intel.com>
2020-07-09 12:06:42 -0700

    Added training script & YAML file

---
+++ train.py
@@ -0,0 +1,205 @@
+import torch
+from torch.utils.data import Dataset
+from torch.utils.data.dataset import TensorDataset
+from torch.distributions.multivariate_normal import MultivariateNormal
+import torch.nn.functional as F
+from torch_geometric.data import DataLoader
+from torch_geometric.data import Data
+from torch_geometric.utils.convert import to_networkx
+
+import networkx as nx # for visualizing graphs
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import pdb
+from torchsummary import summary
+import os 
+import argparse
+
+from functions.load_data import MarielDataset, edges
+from functions.functions import *
+from functions.modules import *
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--name', type=str, default="vae", help='Distinguishing prefix for save directory.')
+parser.add_argument('--epochs', type=int, default=100, help='Number of epochs to train.')
+parser.add_argument('--batch_size', type=int, default=32, help='Number of sequences per batch.')
+parser.add_argument('--seq_len', type=int, default=49, help='Number of timesteps per sequence.')
+parser.add_argument('--predicted_timesteps', type=int, default=10, help='Number of timesteps to predict.')
+parser.add_argument('--batch_limit', type=int, default=0, help='Number of batches per epoch -- if 0, will run over all batches.')
+parser.add_argument('--reduced_joints', action='store_true', default=False, help='Trains on 18 joints rather than all 53.')
+args = parser.parse_args()
+print(args)
+
+### LOAD DATA
+data = MarielDataset(seq_len=args.seq_len, reduced_joints=args.reduced_joints, predicted_timesteps=args.predicted_timesteps)
+dataloader = DataLoader(data, batch_size=args.batch_size, shuffle=False, drop_last=True)
+print("\nGenerated {:,} batches of shape: {}".format(len(dataloader), data[0]))
+
+### DEFINE MODEL 
+node_features = data.seq_len*data.n_dim
+edge_features = data[0].num_edge_features
+node_embedding_dim = 25
+edge_embedding_dim = 4 # number of edge types
+hidden_size = 50
+num_layers = 2
+checkpoint_loaded = False 
+if args.reduced_joints: 
+    n_joints = 18
+else:
+    n_joints = 53
+
+
+# checkpoint_path = "weights/checkpoint_{}joints_seqlen{}_pred{}.pth".format(data[0].num_nodes, args.seq_len, args.predicted_timesteps)
+save_folder = os.path.join("logs",args.name+"_{}joints_seqlen{}_pred{}".format(data[0].num_nodes, args.seq_len, args.predicted_timesteps))
+print("Saving to {}".format(save_folder))
+os.makedirs(save_folder)
+checkpoint_path = os.path.join(save_folder,"checkpoint.pth")
+log_file = os.path.join(save_folder, 'log.txt')
+log = open(log_file, 'w')
+print(args, file=log)
+log.flush()
+
+model = VAE(node_features=node_features, 
+            edge_features=edge_features, 
+            hidden_size=hidden_size, 
+            node_embedding_dim=node_embedding_dim,
+            edge_embedding_dim=edge_embedding_dim,
+            num_layers=num_layers,
+            input_size=node_embedding_dim, 
+            output_size=node_features+args.predicted_timesteps*data.n_dim,
+           )
+
+optimizer = torch.optim.Adam(list(model.parameters()), lr=1e-4, weight_decay=5e-4)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print("Using {}".format(device))
+model = model.to(device)
+print(model)
+print("Total trainable parameters: {:,}".format(count_parameters(model)))
+
+### LOAD PRE-TRAINED WEIGHTS
+if os.path.isfile(checkpoint_path):
+    print("Loading saved checkpoint from {}...".format(checkpoint_path))
+    checkpoint = torch.load(checkpoint_path)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    epoch = checkpoint['epoch']
+    loss_checkpoint = checkpoint['loss']
+    checkpoint_loaded = True
+
+### TRAIN
+mse_loss = torch.nn.MSELoss(reduction='mean')
+prediction_to_reconstruction_loss_ratio = 0 # you might want to weight the prediction loss higher to help it compete with the larger prediction seq_len
+sigma = 0.001 # how to pick sigma?
+
+def train(epochs):
+    losses = []
+    reconstruction_losses = []
+    prediction_losses = []
+    inputs = []
+    outputs = []
+    model.train()
+    for epoch in range(epochs):
+        average_loss = 0
+        average_reconstruction_loss = 0
+        average_prediction_loss = 0
+        i = 0
+        batch_inputs = []
+        batch_outputs = []
+        
+        for batch in tqdm(dataloader, desc="Batches"):
+            batch = batch.to(device)
+            optimizer.zero_grad() # reset the gradients to zero
+            
+            ### CALCULATE MODEL OUTPUTS
+            output = model(batch)
+            batch_inputs.append(batch.x)
+            batch_outputs.append(output)
+            
+            ### CALCULATE LOSS
+            reconstruction_loss = mse_loss(batch.x.to(device), output[:,:node_features]) # compare first seq_len timesteps
+            average_reconstruction_loss += reconstruction_loss.item()
+            batch_loss = reconstruction_loss
+            if args.predicted_timesteps > 0: 
+                prediction_loss = mse_loss(batch.y.to(device), output[:,node_features:]) # compare last part to unseen data
+                batch_loss += prediction_to_reconstruction_loss_ratio*prediction_loss
+                average_prediction_loss += prediction_loss.item()
+
+            ### BACKPROPAGATE
+            batch_loss.backward()
+            optimizer.step()
+            average_loss += batch_loss.item()
+
+            i += 1
+            if args.batch_limit > 0: if i >= args.batch_limit: break # temporary -- for stopping training early
+                
+        inputs.append(torch.stack(batch_inputs))
+        outputs.append(torch.stack(batch_outputs))
+        
+        average_loss = average_loss / i # use len(dataloader) for full batches
+        average_reconstruction_loss = average_reconstruction_loss / i # use len(dataloader) for full batches
+        average_prediction_loss = average_prediction_loss / i # use len(dataloader) for full batches
+
+        losses.append(average_loss) 
+        reconstruction_losses.append(average_reconstruction_loss)
+        prediction_losses.append(average_prediction_loss)
+        print("epoch : {}/{} | Loss = {:,.4f} | Reconstruction Loss: {:,.4f} | Prediction Loss: {:,.4f}".format(epoch+1, epochs, 
+                                                                                                                average_loss,
+                                                                                                                average_reconstruction_loss, 
+                                                                                                                average_prediction_loss))
+        
+        if epoch == 0 and not checkpoint_loaded: best_loss = average_loss
+        elif epoch == 0 and checkpoint_loaded: best_loss = min(average_loss, loss_checkpoint)
+            
+        if average_loss < best_loss:
+            best_loss = average_loss
+            torch.save({
+             'epoch': epoch,
+             'model_state_dict': model.state_dict(),
+             'optimizer_state_dict': optimizer.state_dict(),
+             'loss': best_loss,
+             }, checkpoint_path)
+            print("Better loss achieved -- saved model checkpoint to {}.".format(checkpoint_path))
+    return losses, reconstruction_losses, prediction_losses, inputs, outputs
+
+losses, reconstruction_losses, prediction_losses, inputs, outputs = train(epochs=args.epochs)
+
+### MAKE PLOTS
+fig, ax = plt.subplots(figsize=(8,6))
+ax.plot(np.arange(len(losses)), reconstruction_losses, label="Reconstruction")
+ax.set_xlabel("Epoch", fontsize=16)
+ax.set_ylabel("Loss", fontsize=16)
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+ax.legend(fontsize=14)
+plt.savefig(os.path.join(save_folder,"losses.jpg"))
+
+### LOOK AT PREDICTIONS
+input = inputs[0]
+input = input.cpu().detach().numpy()
+first_input_batch = input[0]
+first_input_seq = first_input_batch[:n_joints, :]
+
+# reshape to be n_joints x n_timesteps x n_dim
+first_input_seq = first_input_seq.reshape((n_joints,args.seq_len,3))
+
+pred = outputs[0]
+pred = pred.cpu().detach().numpy() # convert to numpy
+first_predicted_batch = pred[0]
+first_predicted_seq = first_predicted_batch[:n_joints, :]
+
+# reshape to be n_joints x n_timesteps x n_dim
+first_predicted_seq = first_predicted_seq.reshape((n_joints,args.seq_len,3))
+
+plt.figure(figsize=(10,7))
+for joint in range(1): # first few joints
+# for joint in range(first_seq.shape[0]): # all joints
+    # plot x & y for the sequence
+    plt.plot(first_input_seq[joint,:,0], first_input_seq[joint,:,1], 'o--', label="Input Joint "+str(joint)) 
+    plt.plot(first_predicted_seq[joint,:,0], first_predicted_seq[joint,:,1], 'o--', label="Predicted Joint "+str(joint)) 
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+plt.legend(fontsize=12)
+plt.savefig(os.path.join(save_folder,"predictions_joint0.jpg"))
